3ccc6f6a7b7b534ecfc202a4f9e42bbd4c6ade25
sergscorpy <sergscorpy@gmail.com>
2026-02-26 13:14:13 +0200

    add mouse events

--- Controls/ServoControllerModuleOverlayForm.cs
+++ Controls/ServoControllerModuleOverlayForm.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 using System.Windows.Forms;
@@ -7,6 +8,16 @@ namespace MissionPlanner.Controls
 {
     public class ServoControllerModuleOverlayForm : Form
     {
+        private const int MaxIconsCount = 4;
+
+        private readonly TableLayoutPanel iconsLayout;
+        private readonly List<PictureBox> icons = new List<PictureBox>();
+        private readonly List<ToolStripMenuItem> iconCountMenuItems = new List<ToolStripMenuItem>();
+
+        private bool isDragging;
+        private Point dragOffset;
+        private int visibleIconsCount = MaxIconsCount;
+
         public ServoControllerModuleOverlayForm()
         {
             Text = "Плата контролю";
@@ -18,21 +29,21 @@ namespace MissionPlanner.Controls
             ShowInTaskbar = false;
             Size = new Size(150, 360);
 
-            var iconsLayout = new TableLayoutPanel
+            iconsLayout = new TableLayoutPanel
             {
                 Dock = DockStyle.Fill,
                 ColumnCount = 1,
-                RowCount = 4,
+                RowCount = MaxIconsCount,
                 Padding = new Padding(20, 15, 20, 15)
             };
 
-            for (var i = 0; i < 4; i++)
+            for (var i = 0; i < MaxIconsCount; i++)
             {
-                iconsLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 25f));
+                iconsLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 100f / MaxIconsCount));
             }
 
             var iconPath = ResolveDropsEmptyImagePath();
-            for (var i = 0; i < 4; i++)
+            for (var i = 0; i < MaxIconsCount; i++)
             {
                 var icon = new PictureBox
                 {
@@ -42,10 +53,119 @@ namespace MissionPlanner.Controls
                     Margin = new Padding(0, 4, 0, 4)
                 };
 
+                icons.Add(icon);
                 iconsLayout.Controls.Add(icon, 0, i);
             }
 
             Controls.Add(iconsLayout);
+
+            ContextMenuStrip = BuildIconsCountMenu();
+            RegisterDragEvents(this);
+            ApplyVisibleIconsCount();
+        }
+
+        private ContextMenuStrip BuildIconsCountMenu()
+        {
+            var menu = new ContextMenuStrip();
+            var titleItem = new ToolStripMenuItem("Кількість іконок")
+            {
+                Enabled = false
+            };
+
+            menu.Items.Add(titleItem);
+            menu.Items.Add(new ToolStripSeparator());
+
+            for (var i = 1; i <= MaxIconsCount; i++)
+            {
+                var count = i;
+                var item = new ToolStripMenuItem(count.ToString())
+                {
+                    CheckOnClick = true
+                };
+
+                item.Click += (_, __) =>
+                {
+                    visibleIconsCount = count;
+                    ApplyVisibleIconsCount();
+                };
+
+                iconCountMenuItems.Add(item);
+                menu.Items.Add(item);
+            }
+
+            return menu;
+        }
+
+        private void ApplyVisibleIconsCount()
+        {
+            for (var i = 0; i < icons.Count; i++)
+            {
+                var isVisible = i < visibleIconsCount;
+                icons[i].Visible = isVisible;
+                iconsLayout.RowStyles[i].SizeType = isVisible ? SizeType.Percent : SizeType.Absolute;
+                iconsLayout.RowStyles[i].Height = isVisible ? 100f / visibleIconsCount : 0f;
+            }
+
+            for (var i = 0; i < iconCountMenuItems.Count; i++)
+            {
+                iconCountMenuItems[i].Checked = i + 1 == visibleIconsCount;
+            }
+        }
+
+        private void RegisterDragEvents(Control control)
+        {
+            control.MouseDown += OnAnyControlMouseDown;
+            control.MouseMove += OnAnyControlMouseMove;
+            control.MouseUp += OnAnyControlMouseUp;
+
+            foreach (Control childControl in control.Controls)
+            {
+                RegisterDragEvents(childControl);
+            }
+        }
+
+        private void OnAnyControlMouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left)
+            {
+                return;
+            }
+
+            var sourceControl = sender as Control;
+            if (sourceControl == null)
+            {
+                return;
+            }
+
+            isDragging = true;
+            var mouseScreenPosition = sourceControl.PointToScreen(e.Location);
+            dragOffset = new Point(mouseScreenPosition.X - Left, mouseScreenPosition.Y - Top);
+        }
+
+        private void OnAnyControlMouseMove(object sender, MouseEventArgs e)
+        {
+            if (!isDragging)
+            {
+                return;
+            }
+
+            var sourceControl = sender as Control;
+            if (sourceControl == null)
+            {
+                return;
+            }
+
+            var mouseScreenPosition = sourceControl.PointToScreen(e.Location);
+            Left = mouseScreenPosition.X - dragOffset.X;
+            Top = mouseScreenPosition.Y - dragOffset.Y;
+        }
+
+        private void OnAnyControlMouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isDragging = false;
+            }
         }
 
         private static string ResolveDropsEmptyImagePath()
