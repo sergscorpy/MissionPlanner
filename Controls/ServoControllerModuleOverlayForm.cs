878aa3c013e82f3eb72f78340e0af333fa266898
sergscorpy <sergscorpy@gmail.com>
2026-02-27 10:45:06 +0200

    add logic and icons

--- Controls/ServoControllerModuleOverlayForm.cs
+++ Controls/ServoControllerModuleOverlayForm.cs
@@ -8,19 +8,39 @@ namespace MissionPlanner.Controls
 {
     public class ServoControllerModuleOverlayForm : Form
     {
-        private const int MaxIconsCount = 8;
+        private const int MaxIconsCount = 6;
         private const int DefaultVisibleIconsCount = 4;
+        private const int MinRcChannel = 5;
+        private const int MaxRcChannel = 16;
+        private const int CommandActivationThreshold = 1700;
+        private const int SafetyActivationThreshold = 1700;
 
         private readonly TableLayoutPanel iconsLayout;
         private readonly List<PictureBox> icons = new List<PictureBox>();
         private readonly List<ToolStripMenuItem> iconCountMenuItems = new List<ToolStripMenuItem>();
+        private readonly List<ToolStripMenuItem> commandChannelMenuItems = new List<ToolStripMenuItem>();
+        private readonly List<ToolStripMenuItem> safetyChannelMenuItems = new List<ToolStripMenuItem>();
+        private readonly List<ToolStripMenuItem> selectionChannelMenuItems = new List<ToolStripMenuItem>();
+        private readonly Timer blinkTimer;
         private readonly Image unlockedImage;
         private readonly Image lockedImage;
+        private readonly Image unlockedOrangeImage;
+        private readonly Image lockedOrangeImage;
+        private readonly Image unlockedRedImage;
+        private readonly Image lockedRedImage;
 
         private bool isDragging;
         private Point dragOffset;
         private int visibleIconsCount = DefaultVisibleIconsCount;
         private int lockMask;
+        private int commandChannel = 5;
+        private int safetyChannel = 6;
+        private int selectionChannel = 7;
+        private readonly ushort[] rcChannelValues = new ushort[18];
+        private bool safetyActive;
+        private bool commandActive;
+        private int selectedIconIndex = -1;
+        private bool blinkIsRed;
 
         public ServoControllerModuleOverlayForm()
         {
@@ -48,6 +68,10 @@ namespace MissionPlanner.Controls
 
             unlockedImage = Image.FromFile(ResolveIconImagePath("Drops_Empty.png"));
             lockedImage = Image.FromFile(ResolveIconImagePath("Drops_Green.png"));
+            unlockedOrangeImage = Image.FromFile(ResolveIconImagePath("Drops_Empty_Orange.png"));
+            lockedOrangeImage = Image.FromFile(ResolveIconImagePath("Drops_Orange.png"));
+            unlockedRedImage = Image.FromFile(ResolveIconImagePath("Drops_Empty_Red.png"));
+            lockedRedImage = Image.FromFile(ResolveIconImagePath("Drops_Red.png"));
 
             for (var i = 0; i < MaxIconsCount; i++)
             {
@@ -65,10 +89,17 @@ namespace MissionPlanner.Controls
 
             Controls.Add(iconsLayout);
 
-            ContextMenuStrip = BuildIconsCountMenu();
+            blinkTimer = new Timer { Interval = 500 };
+            blinkTimer.Tick += (_, __) =>
+            {
+                blinkIsRed = !blinkIsRed;
+                ApplyOverlayState();
+            };
+
+            ContextMenuStrip = BuildContextMenu();
             RegisterDragEvents(this);
             ApplyVisibleIconsCount();
-            ApplyLockMask();
+            ApplyOverlayState();
         }
 
         public void UpdateLockMask(int newLockMask)
@@ -82,28 +113,46 @@ namespace MissionPlanner.Controls
 
             if (InvokeRequired)
             {
-                BeginInvoke((Action)ApplyLockMask);
+                BeginInvoke((Action)ApplyOverlayState);
                 return;
             }
 
-            ApplyLockMask();
+            ApplyOverlayState();
         }
 
-        private ContextMenuStrip BuildIconsCountMenu()
+        public void UpdateRcChannels(ushort[] channels)
+        {
+            if (channels == null)
+            {
+                return;
+            }
+
+            if (IsDisposed)
+            {
+                return;
+            }
+
+            if (InvokeRequired)
+            {
+                BeginInvoke((Action)(() => UpdateRcChannels(channels)));
+                return;
+            }
+
+            var count = Math.Min(rcChannelValues.Length, channels.Length);
+            Array.Copy(channels, rcChannelValues, count);
+
+            UpdateStateFromRcChannels();
+        }
+
+        private ContextMenuStrip BuildContextMenu()
         {
             var menu = new ContextMenuStrip();
-            var titleItem = new ToolStripMenuItem("Кількість іконок")
-            {
-                Enabled = false
-            };
 
-            menu.Items.Add(titleItem);
-            menu.Items.Add(new ToolStripSeparator());
-
+            var iconsCountItem = new ToolStripMenuItem("Кількість іконок");
             for (var i = 1; i <= MaxIconsCount; i++)
             {
                 var count = i;
-                var item = new ToolStripMenuItem(count.ToString())
+                var item = new ToolStripMenuItem($"Іконка {count}")
                 {
                     CheckOnClick = true
                 };
@@ -115,10 +164,70 @@ namespace MissionPlanner.Controls
                 };
 
                 iconCountMenuItems.Add(item);
-                menu.Items.Add(item);
+                iconsCountItem.DropDownItems.Add(item);
             }
 
+            var commandChannelItem = BuildRcChannelMenuItem("Канал команди", commandChannelMenuItems, channel =>
+            {
+                commandChannel = channel;
+                UpdateStateFromRcChannels();
+            });
+
+            var safetyChannelItem = BuildRcChannelMenuItem("Канал запобіжника", safetyChannelMenuItems, channel =>
+            {
+                safetyChannel = channel;
+                UpdateStateFromRcChannels();
+            });
+
+            var selectionChannelItem = BuildRcChannelMenuItem("Канал вибору", selectionChannelMenuItems, channel =>
+            {
+                selectionChannel = channel;
+                UpdateStateFromRcChannels();
+            });
+
+            menu.Items.Add(iconsCountItem);
+            menu.Items.Add(commandChannelItem);
+            menu.Items.Add(safetyChannelItem);
+            menu.Items.Add(selectionChannelItem);
+
+            UpdateRcChannelMenuChecks(commandChannelMenuItems, commandChannel);
+            UpdateRcChannelMenuChecks(safetyChannelMenuItems, safetyChannel);
+            UpdateRcChannelMenuChecks(selectionChannelMenuItems, selectionChannel);
+
             return menu;
+        }
+
+        private ToolStripMenuItem BuildRcChannelMenuItem(string title, List<ToolStripMenuItem> menuItems, Action<int> onSelected)
+        {
+            var item = new ToolStripMenuItem(title);
+
+            for (var channel = MinRcChannel; channel <= MaxRcChannel; channel++)
+            {
+                var rcChannel = channel;
+                var channelItem = new ToolStripMenuItem($"Канал {rcChannel}")
+                {
+                    CheckOnClick = true
+                };
+
+                channelItem.Click += (_, __) =>
+                {
+                    onSelected(rcChannel);
+                    UpdateRcChannelMenuChecks(menuItems, rcChannel);
+                };
+
+                menuItems.Add(channelItem);
+                item.DropDownItems.Add(channelItem);
+            }
+
+            return item;
+        }
+
+        private void UpdateRcChannelMenuChecks(List<ToolStripMenuItem> menuItems, int selectedChannel)
+        {
+            foreach (var item in menuItems)
+            {
+                item.Checked = item.Text.EndsWith(selectedChannel.ToString());
+            }
         }
 
         private void ApplyVisibleIconsCount()
@@ -136,16 +245,82 @@ namespace MissionPlanner.Controls
                 iconCountMenuItems[i].Checked = i + 1 == visibleIconsCount;
             }
 
-            ApplyLockMask();
+            ApplyOverlayState();
         }
 
-        private void ApplyLockMask()
+        private void UpdateStateFromRcChannels()
+        {
+            safetyActive = ReadRcChannelValue(safetyChannel) > SafetyActivationThreshold;
+            commandActive = ReadRcChannelValue(commandChannel) > CommandActivationThreshold;
+
+            var selectionChannelValue = ReadRcChannelValue(selectionChannel);
+            selectedIconIndex = ResolveSelectedIconIndex(selectionChannelValue);
+
+            blinkTimer.Enabled = safetyActive && commandActive && selectedIconIndex >= 0;
+            if (!blinkTimer.Enabled)
+            {
+                blinkIsRed = false;
+            }
+
+            ApplyOverlayState();
+        }
+
+        private static int ResolveSelectedIconIndex(int channelValue)
+        {
+            if (channelValue >= 801 && channelValue <= 1100)
+                return 0;
+            if (channelValue >= 1101 && channelValue <= 1300)
+                return 1;
+            if (channelValue >= 1301 && channelValue <= 1500)
+                return 2;
+            if (channelValue >= 1501 && channelValue <= 1700)
+                return 3;
+            if (channelValue >= 1701 && channelValue <= 1900)
+                return 4;
+            if (channelValue >= 1901 && channelValue <= 2200)
+                return 5;
+
+            return -1;
+        }
+
+        private int ReadRcChannelValue(int channelNumber)
+        {
+            var index = channelNumber - 1;
+            if (index < 0 || index >= rcChannelValues.Length)
+            {
+                return 0;
+            }
+
+            return rcChannelValues[index];
+        }
+
+        private void ApplyOverlayState()
         {
             for (var i = 0; i < icons.Count; i++)
             {
                 var isLocked = (lockMask & (1 << i)) != 0;
-                icons[i].Image = isLocked ? lockedImage : unlockedImage;
+                var isSelectedIcon = safetyActive && selectedIconIndex == i;
+
+                if (isSelectedIcon)
+                {
+                    var showRed = commandActive && blinkIsRed;
+                    icons[i].Image = ResolveSelectedIconImage(isLocked, showRed);
+                }
+                else
+                {
+                    icons[i].Image = isLocked ? lockedImage : unlockedImage;
+                }
             }
+        }
+
+        private Image ResolveSelectedIconImage(bool isLocked, bool showRed)
+        {
+            if (showRed)
+            {
+                return isLocked ? lockedRedImage : unlockedRedImage;
+            }
+
+            return isLocked ? lockedOrangeImage : unlockedOrangeImage;
         }
 
         private void RegisterDragEvents(Control control)
@@ -210,6 +385,11 @@ namespace MissionPlanner.Controls
             {
                 unlockedImage?.Dispose();
                 lockedImage?.Dispose();
+                unlockedOrangeImage?.Dispose();
+                lockedOrangeImage?.Dispose();
+                unlockedRedImage?.Dispose();
+                lockedRedImage?.Dispose();
+                blinkTimer?.Dispose();
             }
 
             base.Dispose(disposing);
--- Utilities/ServoControllerModuleOverlayService.cs
+++ Utilities/ServoControllerModuleOverlayService.cs
@@ -15,6 +15,7 @@ namespace MissionPlanner.Utilities
         private bool moduleDetected;
         private bool disposed;
         private int lockMask;
+        private readonly ushort[] rcChannels = new ushort[18];
 
         public ServoControllerModuleOverlayService(Form owner, MAVLinkInterface mavLinkInterface)
         {
@@ -42,6 +43,12 @@ namespace MissionPlanner.Utilities
             if (messageId == MAVLink.MAVLINK_MSG_ID.NAMED_VALUE_INT)
             {
                 HandleNamedValueInt(message);
+                return;
+            }
+
+            if (messageId == MAVLink.MAVLINK_MSG_ID.RC_CHANNELS)
+            {
+                HandleRcChannels(message);
             }
         }
 
@@ -86,6 +93,42 @@ namespace MissionPlanner.Utilities
             }));
         }
 
+        private void HandleRcChannels(MAVLink.MAVLinkMessage message)
+        {
+            if (message.sysid != 1 || message.compid != (byte)MAVLink.MAV_COMPONENT.MAV_COMP_ID_PERIPHERAL)
+            {
+                return;
+            }
+
+            var rc = message.ToStructure<MAVLink.mavlink_rc_channels_t>();
+            rcChannels[0] = rc.chan1_raw;
+            rcChannels[1] = rc.chan2_raw;
+            rcChannels[2] = rc.chan3_raw;
+            rcChannels[3] = rc.chan4_raw;
+            rcChannels[4] = rc.chan5_raw;
+            rcChannels[5] = rc.chan6_raw;
+            rcChannels[6] = rc.chan7_raw;
+            rcChannels[7] = rc.chan8_raw;
+            rcChannels[8] = rc.chan9_raw;
+            rcChannels[9] = rc.chan10_raw;
+            rcChannels[10] = rc.chan11_raw;
+            rcChannels[11] = rc.chan12_raw;
+            rcChannels[12] = rc.chan13_raw;
+            rcChannels[13] = rc.chan14_raw;
+            rcChannels[14] = rc.chan15_raw;
+            rcChannels[15] = rc.chan16_raw;
+            rcChannels[16] = rc.chan17_raw;
+            rcChannels[17] = rc.chan18_raw;
+
+            owner.BeginInvokeIfRequired((Action)(() =>
+            {
+                if (overlayForm != null && !overlayForm.IsDisposed)
+                {
+                    overlayForm.UpdateRcChannels(rcChannels);
+                }
+            }));
+        }
+
         private void ShowOverlay()
         {
             if (disposed)
@@ -97,6 +140,7 @@ namespace MissionPlanner.Utilities
             {
                 overlayForm = new ServoControllerModuleOverlayForm();
                 overlayForm.UpdateLockMask(lockMask);
+                overlayForm.UpdateRcChannels(rcChannels);
                 overlayForm.Show(owner);
             }
             else
